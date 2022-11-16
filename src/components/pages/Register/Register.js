1152c75d3a6502694e6ca2b928e0d49d1503ee7f
bekicom <adelkauz6@gmail.com>
2022-11-16 14:21:06 +0500

    register

--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -1,33 +1,78 @@
-import React from 'react'
-import { useState } from 'react';
-import logo from "../../../asets/nav/Vector.png"
-import "./register.css"
+import React from "react";
+import logo from "../../../asets/nav/Vector.png";
+import "./register.css";
+import { useState } from "react";
+import { PatternFormat } from "react-number-format";
+import axios from "axios";
 
-export  function Register() {
-
-  const[open,setOpen]=useState(true)
+export function Register() {
+  const api = "https://algoritm21.pandashop.uz";
+  const [name, setName] = useState({});
+  const [phone, setPhone] = useState({});
+  const [course, setCourse] = useState({});
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="register" style={open ? { display: " flex" } : {}}>
-      <form action="">
-        {/* <span id="close" >
-          x
-        </span> */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          axios(`${api}/message`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            data: JSON.stringify({
+              name,
+              phone,
+              course,
+            }),
+          })
+            .then((res) => {
+              console.log(res.data);
+            })
+            .catch((err) => {
+              console.log(err.response.data);
+            });
+        }}
+      >
         <div className="logo_r">
           <img src={logo} alt="" />
-          <p>R’yxatdan otish</p>
+          {/* <p>R’yxatdan otish</p> */}
         </div>
-        <input type="text" placeholder="ism" />
-        <input type="text" placeholder="familya" />
-        <input type="text" placeholder="tel" />
-        <select name="" id="">
-          <option value="">Kursni tanlang</option>
-          <option value="">Dasturlash</option>
-          <option value="">Rus-tili</option>
-          <option value="">Ingliz-tili</option>
-          <option value="">DTM</option>
+        <input
+          required
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          type="text"
+          placeholder="ism"
+        />
+        <PatternFormat
+          required
+          format="+998 (##) ### ####"
+          allowEmptyFormatting
+          mask="_"
+          onValueChange={(e) => {
+            setPhone(e.value);
+          }}
+        />
+
+        <select
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+          name=""
+          id=""
+        >
+          <option value="Kursni tanlang">Kursni tanlang</option>
+          <option value="Dasturlash">Dasturlash</option>
+          <option value="Rus-tili">Rus-tili</option>
+          <option value="Ingliz-tili">Ingliz-tili</option>
+          <option value="DTM">DTM</option>
         </select>
-        <button  onClick={()=> setOpen(false)} >Login</button>
+        <button type="submit" onClick={() => setOpen(false)}>
+          Login
+        </button>
       </form>
     </div>
   );
